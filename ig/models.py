9482f4ea53ca96fd9418d9e9446e75151a3af29a
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-01 13:02:08 +0300

    create a class Image model

--- ig/models.py
+++ ig/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    
+    image = models.ImageField(upload_to = 'media/',default="")
+    imageName = models.CharField(length=60) 
+    imageCaption = models.CharField(length=60)
+    ProfileForeignkey = 
+    Likes
+    Comments
--- ig/urls.py
+++ ig/urls.py
@@ -4,8 +4,8 @@ from django.urls import path
 urlpatterns = [
     path('', views.home , name ='home'),
     path('landing/',views.landing_page,name='landing'),
-    path('details/', views.details ,name='details'),
-    path('profile/', views.profile, name='profile'),
+    path('details/', views.details,name='details'),
+    path('profile/', views.profile,name='profile'),
     path('search/' ,views.search, name='search' )
 
 ]
