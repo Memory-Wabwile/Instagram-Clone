bcc95c63ce57ae4dc00ab4780ad07c95f8f7768d
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-01 13:42:56 +0300

    add class image and profile to the models

--- ig/models.py
+++ ig/models.py
@@ -1,11 +1,29 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    
+
+class Profile(models.Model):
+    profilePhoto = models.ImageField(upload_to = 'media/',default="") 
+    name = models.CharField(length=150)
+    username = models.CharField(length=150)
+    bio = models.CharField(length=150)
+
+
+class Comments(models.Model):
+    comment=models.CharField() 
+
+
+class Image(models.Model):    
     image = models.ImageField(upload_to = 'media/',default="")
     imageName = models.CharField(length=60) 
     imageCaption = models.CharField(length=60)
-    ProfileForeignkey = 
-    Likes
-    Comments
+    profile = models.ForeignKey(Profile,on_delete= models.CASCADE)
+    likes= models.PositiveIntegerField(default = 0)
+    comments = models.ManyToManyField(Comments,on_delete= models.CASCADE,default=False)
+
+
+
+
+
+
+
