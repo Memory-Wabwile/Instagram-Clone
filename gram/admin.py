ff20c65646175b94be7b74871a7c0664d7d96923
Memory-Wabwile <memorywabwile@gmail.com>
2022-04-02 12:48:49 +0300

    register the models in the admin file

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from gram.models import Comments, Image, Profile
+
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Comments)
+admin.site.register(Profile)
